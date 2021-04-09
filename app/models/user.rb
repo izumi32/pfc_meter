84cde8d6358f4ec7f3bc060fa1ccc7dd59bfff80
izumi32 <k.izumi721@gmail.com>
2021-04-09 23:06:49 +0900

    userモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "nameが空の時エラーを返す" do
-    user = User.new(name: "", email: "test@example.com")
-    expect(user).to_not be_valid
+  
+  describe "name" do
+    it "nameが空の時エラーを返す" do
+      user = User.new(name: "",
+                      email: "test@example.com",
+                      password: "password",
+                      password_confirmation: "password")
+      expect(user).to_not be_valid
+    end
   end
 
-  it "emailが空の時エラーを返す" do
-    user = User.new(name: "test user", email: "")
-    expect(user).to_not be_valid
+  describe "email" do
+    it "emailが空の時エラーを返す" do
+      user = User.new(name: "test user",
+                      email: "",
+                      password: "password",
+                      password_confirmation: "password")
+      expect(user).to_not be_valid
+    end
+
+    it "emailが無効の時エラーを返す" do
+      invalid_address = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+      invalid_address.each do |invalid_address|
+        user = User.new(name: "test user",
+                        email: invalid_address,
+                        password: "password",
+                        password_confirmation: "password")
+        expect(user).to_not be_valid
+      end
+    end
+
+    it "emailが重複する時エラーを返す" do
+      user = User.create(name: "test user",
+                      email: "test@example.com",
+                      password: "password",
+                      password_confirmation: "password")
+      duplicate_user = user.dup
+      expect(duplicate_user).to_not be_valid
+    end
+  end
+
+  describe "password" do
+    it "passwordが空の時エラーを返す" do
+      user = User.new(name: "test user",
+                      email: "test@example.com",
+                      password: "")
+      expect(user).to_not be_valid
+    end
+
+    it "password_confirmationが空の時エラーを返す" do
+      user = User.new(name: "test user",
+                      email: "test@example.com",
+                      password: "password",
+                      password_confirmation: "")
+      expect(user).to_not be_valid
+    end
   end
 end
