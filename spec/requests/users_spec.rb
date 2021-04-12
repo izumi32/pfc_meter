ba61a91eddb953b9d971b95a5ea23c500b2d1872
izumi32 <k.izumi721@gmail.com>
2021-04-12 17:11:40 +0900

    リファクタリング

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,34 +4,27 @@ RSpec.describe "Users", type: :request do
 
   let(:user) { FactoryBot.create(:user) }
 
-  context "sign_up" do
-    it "新規登録" do
-      post user_registration_path, params: { name: user.name,
-                                             email: user.email,
-                                             password: user.password,
-                                             password_confirmation: user.password }
-      expect(response).to redirect_to user
-    end
+  it "新規登録" do
+    post user_registration_path, params: { name: user.name,
+                                           email: user.email,
+                                           password: user.password,
+                                           password_confirmation: user.password }
+    expect(response).to redirect_to user
   end
 
-  context "sign_in" do
-    it "ログイン" do
-      post user_session_path, params: { email: user.email, password: user.password }
-      expect(response).to redirect_to user
-    end
+  it "ログイン" do
+    post user_session_path, params: { email: user.email, password: user.password }
+    expect(response).to redirect_to user
   end
 
-  context "#show" do
-    it "userページにアクセス" do
-      get user_path(user)
-      expect(response).to have_http_status(200)
-    end
+  it "userページにアクセス" do
+    get user_path(user)
+    expect(response).to have_http_status(200)
   end
 
-  context "#edit" do
-    it "編集ページにアクセス" do
-      get edit_user_path(user)
-      expect(response).to have_http_status(200)
-    end
+  it "編集ページにアクセス" do
+    get edit_user_path(user)
+    expect(response).to have_http_status(200)
   end
+
 end
