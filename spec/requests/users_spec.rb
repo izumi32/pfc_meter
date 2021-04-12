c2439ba907372ba9b442c27f0fa529f8ffec170a
izumi32 <k.izumi721@gmail.com>
2021-04-12 17:09:28 +0900

    新規登録、ログイン時のリダイレクト先変更

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,6 +4,23 @@ RSpec.describe "Users", type: :request do
 
   let(:user) { FactoryBot.create(:user) }
 
+  context "sign_up" do
+    it "新規登録" do
+      post user_registration_path, params: { name: user.name,
+                                             email: user.email,
+                                             password: user.password,
+                                             password_confirmation: user.password }
+      expect(response).to redirect_to user
+    end
+  end
+
+  context "sign_in" do
+    it "ログイン" do
+      post user_session_path, params: { email: user.email, password: user.password }
+      expect(response).to redirect_to user
+    end
+  end
+
   context "#show" do
     it "userページにアクセス" do
       get user_path(user)
