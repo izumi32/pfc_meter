eef9d8fa8a06d0973e16b3e819c088a92f9eda24
izumi32 <k.izumi721@gmail.com>
2021-04-26 23:35:58 +0900

    過去の登録の統合テスト

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -14,19 +14,20 @@ RSpec.feature "Users", type: :feature do
 
     click_link "食材登録"
 
-    fill_in "食材名", with: "昨日の登録", match: :first
+    fill_in "食材名", with: "今日の登録", match: :first
     fill_in "タンパク質", with: 20, match: :first
     fill_in "脂質", with: 20, match: :first
     fill_in "炭水化物", with: 20, match: :first
-    fill_in "日付", with: Time.zone.yesterday, match: :first
+    fill_in "日付", with: Time.zone.now, match: :first
     fill_in "量", with: 100, match: :first
     click_button "登録"
 
     click_link "過去の登録"
 
-    fill_in "日付", with: Time.zone.yesterday
+    fill_in "日付", with: Time.zone.today
     click_button "表示"
-    
-    expect(page).to have_content "昨日の登録"
+
+    expect(page).to have_content Time.zone.today.strftime("%Y年%m月%d日")
+    expect(page).to have_content "今日の登録"
   end
 end
