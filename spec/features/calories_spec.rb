f254cf43becefd6492e5dee67ece26585935cbf0
izumi32 <k.izumi721@gmail.com>
2021-06-21 18:30:06 +0900

    calorieのフィーチャーspec

---
+++ spec/features/calories_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "Calories", type: :feature do
+
+  let(:user) { FactoryBot.create(:user) }
+
+  before do
+    visit root_path
+    click_link "ログイン"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+  end
+
+  scenario "目安カロリー登録" ,focus: true do
+
+    click_link "カロリー設定"
+    fill_in "タンパク質", with: 130
+    fill_in "脂質", with: 60
+    fill_in "炭水化物", with: 350
+
+    click_button "登録"
+
+    expect(user.calorie).to be_valid
+  end
+end
