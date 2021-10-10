af743c51ae1f63d4c84c6097cbb935cc22103ed1
izumi32 <k.izumi721@gmail.com>
2021-10-11 00:06:37 +0900

    userモデル編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,15 @@ class User < ApplicationRecord
     Thread.current[:user] = user
   end
 
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   private
 
   def downcase_email
