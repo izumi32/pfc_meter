ddf7187f81a886ad8f0bcddd202cd0a9d04e940e
izumi32 <k.izumi721@gmail.com>
2021-10-10 23:54:39 +0900

    userモデル編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
@@ -16,5 +19,17 @@ class User < ApplicationRecord
 
   def self.current=(user)
     Thread.current[:user] = user
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+    # => self.email = self.email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
