0ccff8f25a5a2b060ebd798c531c5c5313051f65
izumi32 <k.izumi721@gmail.com>
2021-10-12 17:38:37 +0900

    registrations#create実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -14,9 +14,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #
-  # end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "認証メールを送信しました"
+      redirect_to root_url
+    else
+      render 'devise/registrations/new'
+    end
+  end
 
   # GET /resource/edit
   # def edit
@@ -63,4 +70,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
