458f544a642340f06d4a91826d8cfb942ac58cd7
izumi32 <k.izumi721@gmail.com>
2021-10-14 22:17:51 +0900

    コード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ApplicationRecord
   end
 
   def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
+    digest = self.send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
