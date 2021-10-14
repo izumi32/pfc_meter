ae8b59edacb3605457317c4a2440fb6b32e92fad
izumi32 <k.izumi721@gmail.com>
2021-10-14 22:28:43 +0900

    有効化の#edit定義

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,5 +1,17 @@
 class AccountActivationsController < ApplicationController
 
   def edit
+    user = User.find_by(email: params[:email])
+    p user
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated, true)
+      user.update_atttibute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "アカウントを有効化しました"
+      redirect_to user
+    else
+      flash[:danger = "有効化できません"]
+      redirect_to root_url
+    end
   end
 end
