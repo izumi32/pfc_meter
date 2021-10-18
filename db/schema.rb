8b996bc4d9e99b5c8ac170ac59dcdbfc96633da3
izumi32 <k.izumi721@gmail.com>
2021-10-19 01:10:59 +0900

    カラム修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_18_080624) do
+ActiveRecord::Schema.define(version: 2021_10_18_155445) do
 
   create_table "calories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "protein"
@@ -44,6 +44,10 @@ ActiveRecord::Schema.define(version: 2021_10_18_080624) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
